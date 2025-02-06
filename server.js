1bebea90f7da46ecbec10c96e549c23efc0ed119
Mykola Saulko <saulkomyk@gmail.com>
2025-02-07 01:05:27 +0200

    install data base for mongodb

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Перевірка наявності MONGODB_URI
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI is not defined in .env file");
+  process.exit(1); // Зупиняємо додаток, якщо змінна відсутня
+}
+
+
 // Підключення до MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
